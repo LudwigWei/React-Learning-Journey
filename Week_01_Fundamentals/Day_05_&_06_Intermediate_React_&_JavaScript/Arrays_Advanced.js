37d073f9c86b2d9809df0acaef6363a4f49ab806
LudwigWei <louis.vncnt.crisaldo@gmail.com>
2025-06-09 12:17:19 +0800

    Added new learning - Advanced Arrays

---
+++ Week_01_Fundamentals/Day_05_&_06_Intermediate_React_&_JavaScript/Arrays_Advanced.js
@@ -0,0 +1,36 @@
+const numbers = [10, 100, 500, 1000, 2000, 3000, 4000];
+
+// push() -------------
+// numbers.push(2000);
+// console.log(numbers);
+
+//  forEach() -------------
+// numbers.forEach((num) => console.log(num + 1));
+
+// map() -------------
+// const newNumbers = numbers.map(function (nr) {
+//   return nr * 2;
+// });
+
+// console.log(newNumbers);
+
+// some() -------------
+// const result = numbers.some(function (nr) {
+//   return nr < 1000;
+// });
+
+// console.log(result);
+
+// find() -------------
+// const result = numbers.find((number) => {
+//   return number > 100;
+// });
+
+// console.log(result);
+
+// filter() -------------
+// const result = numbers.filter((number) => {
+//   return number > 500;
+// });
+
+// console.log(result);
