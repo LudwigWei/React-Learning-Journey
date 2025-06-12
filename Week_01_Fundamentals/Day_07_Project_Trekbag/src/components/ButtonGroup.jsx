059d7af72f3d697feaae779048d308d63f3dd961
LudwigWei <louis.vncnt.crisaldo@gmail.com>
2025-06-12 20:09:02 +0800

    Update

---
+++ Week_01_Fundamentals/Day_07_Project_Trekbag/src/components/ButtonGroup.jsx
@@ -0,0 +1,37 @@
+import Button from "./Button";
+
+export default function ButtonGroup({
+  handleRemoveAllItems,
+  handleResetToInitial,
+  handleMarkAllAsComplete,
+  handleMarkAllAsIncomplete,
+}) {
+  const secondaryButtons = [
+    {
+      text: "Mark all as complete",
+      onClick: handleMarkAllAsComplete,
+    },
+    {
+      text: "Mark all as incomplete",
+      onClick: handleMarkAllAsIncomplete,
+    },
+    {
+      text: "Reset to initial",
+      onClick: handleResetToInitial,
+    },
+    {
+      text: "Remove all items",
+      onClick: handleRemoveAllItems,
+    },
+  ];
+
+  return (
+    <section className="button-group">
+      {secondaryButtons.map(({ text, onClick }) => (
+        <Button key={text} buttonType="secondary" onClick={onClick}>
+          {text}
+        </Button>
+      ))}
+    </section> 
+  );  
+}
