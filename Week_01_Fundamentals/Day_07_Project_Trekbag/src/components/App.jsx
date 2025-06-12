005f89902be2759a7e70f943c8892ceff02759bb
LudwigWei <louis.vncnt.crisaldo@gmail.com>
2025-06-12 19:57:15 +0800

    Added progress

--- Week_01_Fundamentals/Day_07_Project_Trekbag/src/components/App.jsx
+++ Week_01_Fundamentals/Day_07_Project_Trekbag/src/components/App.jsx
@@ -8,6 +8,42 @@ import Sidebar from "./Sidebar";
 
 function App() {
   const [items, setItems] = useState(initialItems);
+
+  const handleAddItem = (newItemText) => {
+    const newItem = {
+      id: new Date().getTime(),
+      name: newItemText,
+      packed: false,
+    };
+    const newItems = [...items, newItem];
+    setItems(newItems);
+  };
+
+  const handleRemoveAllItems = () => {
+    setItems([]);
+  };
+
+  const handleResetToInitial = () => {
+    setItems(initialItems);
+  };
+
+  const handleMarkAllAsComplete = () => {
+    const newItems = items.map((item) => {
+      return { ...item, packed: true };
+    });
+
+    setItems(newItems);
+  };
+  const handleMarkAllAsIncomplete = () => {
+    const newItems = items.map((item) => {
+      return { ...item, packed: false };
+    });
+
+    setItems(newItems);
+  };
+
+  
+
   return (
     <>
       <BackgroundHeading />
@@ -15,7 +51,13 @@ function App() {
       <main>
         <Header />
         <ItemList items={items} />
-        <Sidebar setItems={setItems} />
+        <Sidebar
+          handleMarkAllAsComplete={handleMarkAllAsComplete}
+          handleMarkAllAsIncomplete={handleMarkAllAsIncomplete}
+          handleResetToInitial={handleResetToInitial}
+          handleRemoveAllItems={handleRemoveAllItems}
+          handleAddItem={handleAddItem}
+        />
       </main>
 
       <Footer />
